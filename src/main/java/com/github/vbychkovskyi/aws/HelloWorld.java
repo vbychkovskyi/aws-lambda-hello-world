215fefbf42d9abd91e863bcf3b96443c50f17564
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-18 13:20:26 +0300

    Update interface.

--- src/main/java/com/github/vbychkovskyi/aws/HelloWorld.java
+++ src/main/java/com/github/vbychkovskyi/aws/HelloWorld.java
@@ -4,10 +4,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class HelloWorld implements RequestHandler<String, String> {
+public class HelloWorld implements RequestHandler<Object, Object> {
 
     @Override
-    public String handleRequest(final String input, final Context context) {
+    public Object handleRequest(final Object input, final Context context) {
         final LambdaLogger logger = context.getLogger();
         logger.log("Received message" + input);
         return "Hello, World";
